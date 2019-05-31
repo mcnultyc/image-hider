57541bfddde32d6dca096c1e001342fa95c6fd00
Carlos McNulty <cmcnul3@uic.edu>
2019-05-31 15:50:03 -0500

    added decode function

--- image_hider.c
+++ image_hider.c
@@ -1,15 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "tif.h"
 
 int main(int argc, char *argv[]){ 
+  int decode = 0;
+  int encode = 0;
+  char *filename_img = NULL;
+  char *filename = NULL;
+  int i;
+  for(i = 1; i < argc; i++){
+    if(strcmp("-d", argv[i]) == 0){
+      decode = 1;
+    }
+    else if(strcmp("-e", argv[i]) == 0){
+      encode = 1;
+    }
+    else{
+      if(!filename_img){
+        filename_img = argv[i];
+      }
+      else if(!filename){
+        filename = argv[i];
+      }
+    }
+  } 
+  if((decode == 0 && encode == 0) ||
+    !filename_img || !filename){
+    fprintf(stderr, "invalid options\n");
+    exit(EXIT_FAILURE); 
+  }
+  if(decode){
+    printf("decoding...\n");
+    printf("image: %s\n", filename_img);
+    printf("output: %s\n", filename);
+    tif_read_data(filename_img, filename);
+  }
+  else if(encode){
+    printf("encoding...\n");
+    printf("image: %s\n", filename_img);
+    printf("input: %s\n", filename);
+    tif_write_data(filename, filename_img);
+  }
 
-  tif_read_data("garbage3.tif", "somegarabeheere.zip");
-  //if(!data){
-    //printf("works\n");
-  //}
-  
+
+
   return 0;
 }
 
--- tif.c
+++ tif.c
@@ -202,10 +202,42 @@ static void get_data(char *filename, TIFDATA *dataptr){
   fclose(fptr);
 }
 
+static void decode(BYTE *decoded, unsigned decoded_size, unsigned bits, 
+                   BYTE *bytes, unsigned bytes_size){
+  if(bits == 0 || 8 % bits != 0){
+    fprintf(stderr, "bits to decode must be powers of 2 <= 8\n");
+    exit(EXIT_FAILURE);
+  }
+  unsigned bit_factor = (8/bits);
+  if(bytes_size != decoded_size * bit_factor){
+    fprintf(stderr, "invalid bytes for bit factor\n");
+    exit(EXIT_FAILURE);
+  }
+  BYTE byte_mask = 0x80;
+  BYTE decode_mask = 0x01;
+  int b;
+  for(b = 1; b < bits; b++){
+    byte_mask |= byte_mask >> 1;
+    decode_mask |= decode_mask << 1;
+  }
+  memset((void*)decoded, 0, decoded_size);
+  int i, j;
+  for(i = 0, j = 0; i < decoded_size && j < bytes_size; i++){
+    BYTE mask = byte_mask;
+    int b;
+    for(b = 0; b < bit_factor; b++, j++){
+      unsigned shift = (bit_factor - (b+1))*bits;
+      BYTE byte = (bytes[j] & decode_mask) << shift;
+      decoded[i] = decoded[i] | byte; 
+      mask >>= bit_factor;
+    } 
+  }
+}
+
 static int read_strip(DWORD strip_offset, DWORD strip_bytes, 
                       DWORD size, DWORD byte_count,
-                      FILE *in, FILE *out){
-  if(fseek(in, strip_offset, SEEK_SET) < 0){
+                      FILE *img, FILE *out){
+  if(fseek(img, strip_offset, SEEK_SET) < 0){
     fprintf(stderr, "error seeking strip offset\n");
     exit(EXIT_FAILURE);
   }
@@ -213,7 +245,7 @@ static int read_strip(DWORD strip_offset, DWORD strip_bytes,
   memset(bytes, 0, sizeof(bytes));
   int i;
   for(i = 0; i < strip_bytes && byte_count < size; i += 2, byte_count++){
-    if(fread((void*)bytes, sizeof(bytes), 1, in) < 1){
+    if(fread((void*)bytes, sizeof(bytes), 1, img) < 1){
       fprintf(stderr, "error reading data\n");
       exit(EXIT_FAILURE);
     }
@@ -227,7 +259,7 @@ static int read_strip(DWORD strip_offset, DWORD strip_bytes,
   return byte_count;
 }
 
-static void read_data_(FILE *img, FILE *out, TIFDATA data){
+static void read_data(FILE *img, FILE *out, TIFDATA data){ 
   if(data.strip_count == 0){
     fprintf(stderr, "image has no data\n");
     exit(EXIT_FAILURE);
@@ -241,50 +273,24 @@ static void read_data_(FILE *img, FILE *out, TIFDATA data){
     fprintf(stderr, "error reading data\n");
     exit(EXIT_FAILURE);
   }
-  //TODO call decode function  
-
-
-  fseek(in, 0, SEEK_END);
-  unsigned size = ftell(in); 
-  fseek(in, 0, SEEK_SET); 
-  BYTE encoded[8];
-  if(fread((void*)encoded, sizeof(encoded), 1, img) < 1){
-    fprintf(stderr, "error reading data\n");
-    exit(EXIT_FAILURE);
-  }
-  encode(encoded, sizeof(encoded), 4, 
-        (BYTE*)&size, sizeof(unsigned));
-  if(img == out){
-    if(fseek(out, -sizeof(encoded), SEEK_CUR) < 0){
-      fprintf(stderr, "error resetting position\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-  if(fwrite((void*)encoded, sizeof(encoded), 1, out) < 1){
-    fprintf(stderr, "error encoding file size\n");
-    exit(EXIT_FAILURE);
-  }
+  unsigned size;
+  decode((BYTE*)&size, sizeof(size), 4, encoded, sizeof(encoded));
   data.strip_bytes[0] -= 8;
   data.strip_offsets[0] += 8;
-  printf("offset: %X\n", data.strip_offsets[0]);
-  printf("size: %X, %d\n", size, size);
-  printf("strip count: %d\n", data.strip_count);
   unsigned byte_count = 0;
   int i;
   for(i = 0; i < data.strip_count && byte_count < size; i++){
-    byte_count = write_strip(data.strip_offsets[i], 
+    byte_count = read_strip(data.strip_offsets[i], 
                   data.strip_bytes[i], 
                   size, byte_count,
-                  in, img, out);
+                  img, out);
   }
-  printf("bytes written: %u\n", byte_count);
 }
 
 
 static int write_strip(DWORD strip_offset, DWORD strip_bytes, 
                        DWORD size, DWORD byte_count,
                        FILE *in, FILE *img, FILE *out){
-  printf("write strip: %d\n", size);
   if(fseek(img, strip_offset, SEEK_SET) < 0){
     fprintf(stderr, "error seeking strip offset\n");
     exit(EXIT_FAILURE);
@@ -315,7 +321,6 @@ static int write_strip(DWORD strip_offset, DWORD strip_bytes,
       exit(EXIT_FAILURE);
     }
   }
-  printf("\tbytes encoded: %d\n", byte_count);
   return byte_count;
 }
 
@@ -326,7 +331,7 @@ static void encode(BYTE *encoded, unsigned encoded_size, unsigned bits,
     exit(EXIT_FAILURE);
   }
   unsigned bit_factor = (8/bits);
-  if(encoded_size != bytes_size * bit_factor){
+  if(encoded_size != bytes_size * bit_factor){// sizes for encoding data
     fprintf(stderr, "invalid bytes for bit factor\n");
     exit(EXIT_FAILURE);
   }
@@ -350,7 +355,7 @@ static void encode(BYTE *encoded, unsigned encoded_size, unsigned bits,
   }
 }
 
-static void write_data_(FILE *in, FILE *img, FILE *out, TIFDATA data){
+static void write_data(FILE *in, FILE *img, FILE *out, TIFDATA data){
   if(data.strip_count == 0){
     fprintf(stderr, "image has no data\n");
     exit(EXIT_FAILURE);
@@ -381,9 +386,6 @@ static void write_data_(FILE *in, FILE *img, FILE *out, TIFDATA data){
   }
   data.strip_bytes[0] -= 8;
   data.strip_offsets[0] += 8;
-  printf("offset: %X\n", data.strip_offsets[0]);
-  printf("size: %X, %d\n", size, size);
-  printf("strip count: %d\n", data.strip_count);
   unsigned byte_count = 0;
   int i;
   for(i = 0; i < data.strip_count && byte_count < size; i++){
@@ -392,297 +394,41 @@ static void write_data_(FILE *in, FILE *img, FILE *out, TIFDATA data){
                   size, byte_count,
                   in, img, out);
   }
-  printf("bytes written: %u\n", byte_count);
 }
 
 
-
-
-
-void tif_read_data(char *filename_in, char *filename_out){
- 
-  printf("reading data\n");
-  FILE *finptr, *foutptr;
-  if(!(finptr = fopen(filename_in, "rb"))){ 
+void tif_read_data(char *filename_img, char *filename_out){
+  FILE *img, *out;
+  if(!(img = fopen(filename_img, "rb"))){ 
     perror("error opening input image");
     exit(EXIT_FAILURE);
   }
-  if(!(foutptr = fopen(filename_out, "wb"))){ 
+  if(!(out = fopen(filename_out, "wb"))){ 
     perror("error opening output file");
     exit(EXIT_FAILURE);
   }
   TIFDATA data;
-  get_data(filename_in, &data);
-  printf("length: %d\n", data.length);
-  printf("width: %d\n", data.width);
-  printf("bits per sample: %d\n", data.bits_per_sample);
-  printf("strip count: %d\n", data.strip_count);
-  printf("rows per strip: %d\n", data.rows_per_strip);
-  printf("offsets:\n");
-  int i;//int i;
-  for(i = 0; i < data.strip_count; i++){
-    printf("[%d]: %X\n", i, data.strip_offsets[i]);
-  }
-  printf("\nstrip bytes:\n");
-  for(i = 0; i < data.strip_count; i++){
-    printf("[%d]: %X\n", i, data.strip_bytes[i]);
-  }
-  printf("in read func\n");
-  DWORD j, nibble_count, size; //, size
-  for(i = 0, nibble_count = 0; i < data.strip_count 
-      && nibble_count < size * 2; i++){
-    DWORD strip_offset = data.strip_offsets[i];
-    DWORD strip_bytes = data.strip_bytes[i];
-    BYTE byte, decoded;
-    if(i == 0){
-      if(fseek(finptr, strip_offset, SEEK_SET) < 0){
-       fprintf(stderr, "error reading data\n");
-       exit(EXIT_FAILURE);
-      }
-    }
-    if(i == 0){
-      BYTE bytes[4];
-      memset((void*)&bytes, 0, sizeof(bytes)); 
-      printf("reading size\n");
-      for(nibble_count = 0; nibble_count < sizeof(DWORD) * 2; nibble_count++){
-        if(fread((void*)&byte, sizeof(BYTE), 1, finptr) < 1){
-          fprintf(stderr, "error reading data\n");
-          exit(EXIT_FAILURE);
-        }
-        if(nibble_count % 2 == 0){
-          BYTE nibble = (byte & 0xF) << 4;
-          bytes[nibble_count/2] |= nibble;
-        }
-        else{
-          BYTE nibble = byte & 0xF;
-          bytes[nibble_count/2] |= nibble;
-        }
-        printf("byte read: %X\n", byte);
-      }
-      nibble_count = 0;
-      size = *((DWORD*)bytes);
-      printf("bytes [0]: %X, [1]: %X,[2]: %X, [3]: %X\n", bytes[0], bytes[1], bytes[2], bytes[3]);
-      printf("%u, %X\n", size, size);
-    }
-    for(j = 0; j < strip_bytes 
-        && nibble_count < size * 2; j++, nibble_count++){
-      if(fread((void*)&byte, sizeof(BYTE), 1, finptr) < 1){
-        fprintf(stderr, "error reading data\n");
-        exit(EXIT_FAILURE);
-      }
-      if(nibble_count % 2 == 0){
-        decoded = 0;
-        BYTE nibble = (byte & 0xF) << 4;
-        decoded |= nibble;
-      }
-      else{
-        BYTE nibble = byte & 0xF;
-        decoded |= nibble;
-        if(fwrite((void*)&decoded, sizeof(BYTE), 1, foutptr) < 1){
-          fprintf(stderr, "error updating image\n");
-          exit(EXIT_FAILURE);
-        }
-      }
-    }     
-  }
+  get_data(filename_img, &data);
+  read_data(img, out, data);
+  free_data(&data);
+  fclose(img);
+  fclose(out);
 }
 
-static void write_data(FILE *foutptr, FILE *finptr, TIFDATA data){
-
-  write_data_(finptr, foutptr, foutptr, data);
-
-  return;
-
-  fseek(finptr, 0, SEEK_END); // seek to end of file
-  DWORD size = ftell(finptr); // get current file pointer
-  fseek(finptr, 0, SEEK_SET); // seek back to beginning of file
-  printf("size: %X\n", size);
-  DWORD i, j, nibble_count;
-  for(i = 0, nibble_count = 0; i < data.strip_count 
-      && nibble_count < size * 2; i++){
-    DWORD strip_offset = data.strip_offsets[i];
-    DWORD strip_bytes = data.strip_bytes[i];
-    BYTE byte, encoded;
-    if(i == 0){
-      if(fseek(foutptr, strip_offset, SEEK_SET) < 0){
-       fprintf(stderr, "error reading data\n");
-       exit(EXIT_FAILURE);
-      }
-    }
-    if(i == 0){
-      BYTE bytes[4];
-      bytes[0] =  size & 0xFF;
-      bytes[1] = (size >> 8)  & 0xFF;
-      bytes[2] = (size >> 16) & 0xFF;
-      bytes[3] = (size >> 24) & 0xFF;
-      for(nibble_count = 0; nibble_count < sizeof(DWORD) * 2; nibble_count++){
-        byte = 0;
-        if(fread((void*)&byte, sizeof(BYTE), 1, foutptr) < 1){
-          fprintf(stderr, "error reading data\n");
-          exit(EXIT_FAILURE);
-        }
-        if(nibble_count % 2 == 0){
-          BYTE nibble = (bytes[nibble_count/2] & 0xF0) >> 4;
-          byte = (byte & 0xF0) | nibble;
-        }
-        else{
-          BYTE nibble = (bytes[nibble_count/2] & 0x0F);
-          byte = (byte & 0xF0) | nibble;        
-        }
-        if(fseek(foutptr, -sizeof(BYTE), SEEK_CUR) < 0){
-          fprintf(stderr, "error seeking\n");
-          exit(EXIT_FAILURE);
-        }
-        if(fwrite((void*)&byte, sizeof(BYTE), 1, foutptr) < 1){
-          fprintf(stderr, "error updating image\n");
-          exit(EXIT_FAILURE);
-        }
-      }
-      nibble_count = 0;
-    }
-    for(j = 0; j < strip_bytes 
-        && nibble_count < size * 2; j++, nibble_count++){
-      byte = 0;
-      if(fread((void*)&byte, sizeof(BYTE), 1, foutptr) < 1){
-        fprintf(stderr, "error reading data\n");
-        exit(EXIT_FAILURE);
-      }
-      BYTE orig_byte = byte;
-      if(nibble_count % 2 == 0){
-        encoded = 0;
-        if(fread((void*)&encoded, sizeof(BYTE), 1, finptr) < 1){
-          fprintf(stderr, "error reading data\n");
-          exit(EXIT_FAILURE);
-        }
-        DWORD pos = ftell(finptr);
-        if(pos - 1 == 0x59FFC){
-          printf("original byte read: %X\n", orig_byte);
-          printf("encoded: %X\n", encoded);
-          printf("image pos: %X\n", ftell(foutptr) - 1);
-        }
-        BYTE nibble = (encoded & 0xF0) >> 4;
-        byte = (byte & 0xF0) | nibble;
-      }
-      else{
-        DWORD pos = ftell(finptr);
-        if(pos - 1 == 0x59FFC){
-          printf("original byte read: %X\n", orig_byte);
-          printf("encoded: %X\n", encoded);
-          printf("image pos: %X\n", ftell(foutptr) - 1);
-        }
-        BYTE nibble = (encoded & 0x0F);
-        byte = (byte & 0xF0) | nibble;        
-      }
-      if(fseek(foutptr, -sizeof(BYTE), SEEK_CUR) < 0){
-        fprintf(stderr, "error seeking\n");
-        exit(EXIT_FAILURE);
-      }
-      DWORD pos = ftell(finptr);
-      if(pos - 1 == 0x59FFC){
-       printf("fully encoded byte: %X\n", byte);
-       printf("written to: %X\n", ftell(foutptr));
-      }
-      DWORD before_pos = ftell(foutptr);
-      if(before_pos == 0xB4008){
-        printf("writing byte: %X to address: %X\n", byte, before_pos);
-      }
-      if(fwrite((void*)&byte, sizeof(BYTE), 1, foutptr) < 1){
-        fprintf(stderr, "error updating image\n");
-        exit(EXIT_FAILURE);
-      }
-      DWORD after_pos = ftell(foutptr);
-      if(before_pos == after_pos){
-        printf("excuse me wtf\n");
-      }
-    }     
-  }
-}
-
-char *tif_get_data(char *filename){
-  /*
+void tif_write_data(char *filename_in, char *filename_img){
   TIFDATA data;
-  get_data(filename, &data);
-  printf("length: %d\n", data.length);
-  printf("width: %d\n", data.width);
-  printf("bits per sample: %d\n", data.bits_per_sample);
-  printf("strip count: %d\n", data.strip_count);
-  printf("rows per strip: %d\n", data.rows_per_strip);
-  printf("offsets:\n");
-  int i;
-  for(i = 0; i < data.strip_count; i++){
-    printf("[%d]: %X\n", i, data.strip_offsets[i]);
+  get_data(filename_img, &data);
+  FILE *in, *img;
+  if(!(in = fopen(filename_in, "rb"))){
+    fprintf(stderr, "error opening input file\n");
+    exit(EXIT_FAILURE);
   }
-  printf("\nstrip bytes:\n");
-  for(i = 0; i < data.strip_count; i++){
-    printf("[%d]: %X\n", i, data.strip_bytes[i]);
+  if(!(img = fopen(filename_img, "rb+"))){
+    fprintf(stderr, "error opening image file\n");
+    exit(EXIT_FAILURE);
   }
+  write_data(in, img, img, data);
   free_data(&data);
-  */
-  TIFHDR hdr;
-  TIFDATA data;
-  read_hdr(filename, &hdr);
-  FILE *fptr;
-  if(!(fptr = fopen(filename, "rb+"))){ 
-    perror("error opening image");
-    exit(EXIT_FAILURE);
-  } 
-  if(fseek(fptr, hdr.ifd_offset, SEEK_SET) < 0){
-    perror("error seeking directory offset");
-    exit(EXIT_FAILURE);
-  }
-  WORD n_entries;
-  if(fread((void*)&n_entries, sizeof(WORD), 1, fptr) < 1){
-    fprintf(stderr, "error reading directory entries\n");
-    exit(EXIT_FAILURE);
-  }
-  int i;
-  for(i = 0; i < n_entries; i++){
-    TIFTAG tag;
-    if(fread((void*)&tag, sizeof(TIFTAG), 1, fptr) < 1){
-      fprintf(stderr, "error reading tag\n");
-      exit(EXIT_FAILURE);
-    }
-    long pos = ftell(fptr); 
-    switch(tag.tag_id){
-      case IMAGE_LENGTH:{
-        data.length = tag.data_offset;
-      }
-      break;
-      case IMAGE_WIDTH:{
-        data.width = tag.data_offset;
-      }
-      break;
-      case ROWSPERSTRIP:{
-        data.rows_per_strip = tag.data_offset; 
-      }
-      break;
-      case STRIP_OFFSETS:{
-        read_strip_offsets(fptr, &data, tag);
-      }
-      break;
-      case STRIP_BYTES:{
-        read_strip_bytes(fptr, &data, tag);
-      }
-      break;
-      case BITSPERSAMPLE:{
-        read_bits_per_sample(fptr, &data, tag);
-        printf("bits per sample: %d\n", data.bits_per_sample);
-      }
-      break;
-    }
-    if(fseek(fptr, pos, SEEK_SET) < 0){
-      fprintf(stderr, "error reading tag data\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-  FILE *finptr = fopen("The-CodeBreakers.zip", "rb");
-  if(!finptr){
-    fprintf(stderr, "file not found\n");
-    exit(EXIT_FAILURE);
-  }
-  write_data(fptr,finptr, data);
-  free_data(&data);
-  fclose(fptr);
-  
-  return NULL;
+  fclose(in);
+  fclose(img);
 }
